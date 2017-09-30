434073e04a8c2d4c8f0ac29176fa3bc89bf2c6f8
zhiluwang <w446987550@gmail.com>
2017-09-30 22:17:48 +1000

    test

--- run.py
+++ run.py
@@ -2,8 +2,18 @@ import bottle
 
 from bottle import route
 
-@bottle.route('/')
+@route('/')
 def hello():
     return "Hello World!"
 
-app = application = bottle.default_app()
+@route('/greet')
+def hello2():
+    return "Greeting!"
+
+# from website: https://community.runabove.com/kb/en/development/how-to-run-bottle-uwsgi-nginx.html
+# Run bottle internal test server when invoked directly ie: non-uxsgi mode
+if __name__ == '__main__':
+    bottle.run(host='0.0.0.0', port=8080)
+# Run bottle in application mode. Required in order to get the application working with uWSGI!
+else:
+    app = application = bottle.default_app()
